e648a11e3592ba33814cebd0939523dc3ab2dbc7
lex <clgo@163.com>
2024-10-12 21:09:30 +0800

    2# class

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="app">
         <h1>你好啊！</h1>
+        <Person/>
     </div>
 </template>
 
 <script lang="ts">
+  import Person from './components/Person.vue';
+
   export default {
-    name: 'App' // component name
+    name: 'App', // component name
+    components: {Person} // register component
   }
 </script>
 

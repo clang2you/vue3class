208386f243bd72904e567c1afee852ca44dfcf4b
lex <clgo@163.com>
2024-10-12 21:57:04 +0800

    6# class

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,43 +2,23 @@
     <div class="person">
         <h2>姓名：{{ name }}</h2>
         <h2>年龄：{{ age }}</h2>
-        <button @click="changeName">修改名字</button>
+        <!-- <button @click="changeName">修改名字</button>
         <button @click="changeAge">修改年龄</button>
-        <button @click="showTel">查看联系方式</button>
+        <button @click="showTel">查看联系方式</button> -->
     </div>
 </template>
 
-<script lang="ts">
-
+<!-- <script lang="ts">
 export default {
-    name: 'Person',
-    setup(){
-        // vue3 中弱化 this，setup 函数中 this 是 undefined
-
-        // data，此时的数据都是非响应式的数据
-        let name = '张三'
-        let age = 18
-        let tel = '1388888888'
-
-        // methods
-        function changeName(){
-            // console.log(1)
-            name = 'Lex Chen'
-        }
-        function changeAge(){
-            // console.log(2)
-            age += 1
-        }
-        function showTel(){
-            alert(tel)
-        }
-
-        // 将数据、方法交出去，模版中才可以使用
-        return {name, age, changeName, changeAge, showTel}
-        // setup 的返回值也可以是一个渲染函数
-        // return ()=> '哈哈'
-    }
+    name: 'Person'
 }
+</script> -->
+
+<script lang="ts" setup name="Person">
+  let name = '张三'
+  let age = 18
+  let tel = '138888888'
+  let address = '北京市昌平区宏福苑.宏福科技园'
 </script>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 组件快捷名称插件
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueSetupExtend()
   ],
   resolve: {
     alias: {

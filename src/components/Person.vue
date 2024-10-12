7ec1924b541bfa6e86b29051dcc7abaea6fc2acd
lex <clgo@163.com>
2024-10-12 22:28:15 +0800

    8# class

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,32 +1,32 @@
 <template>
     <div class="person">
-        <h2>姓名：{{ name }}</h2>
-        <h2>年龄：{{ age }}</h2>
-        <h2>地址：{{ address }}</h2>
-        <button @click="changeName">修改名字</button>
-        <button @click="changeAge">修改年龄</button>
-        <button @click="showTel">查看联系方式</button>
+        <h2>汽车信息：一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
+        <button @click="changePrice">修改汽车的价格</button>
+        <br>
+        <h2>游戏列表：</h2>
+        <ul>
+          <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+        </ul>
+        <button @click="changeFirstGame">修改第一个游戏的名字</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-  import {ref} from 'vue'
+import { reactive } from 'vue';
 
-  let name = ref('张三')
-  let age = ref(18)
-  let tel = '138888888'
-  let address = '北京市昌平区宏福苑.宏福科技园'
+// data
+let car = reactive({ brand: 'Mercedes Benz', price: 100 })
+let games = reactive([{ id: '#1', name: '王者荣耀' }, { id: '#2', name: '原神' }, { id: '#3', name: '三国志' }])
 
-  function changeName(){
-    name.value = "Lex Chen"
-  }
-  function changeAge(){
-    age.value += 1
-  }
-  function showTel(){
-    alert(tel)
-  }
+// console.log(car)
 
+// methods
+function changePrice() {
+    car.price += 10
+}
+function changeFirstGame(){
+  games[0].name = '杀戮地带'
+}
 </script>
 
 <style scoped>
@@ -36,7 +36,12 @@
     border-radius: 10px;
     padding: 20px;
 }
+
 button {
     margin: 0 5px;
+}
+
+ul {
+  font-size: 20px;
 }
 </style>

16b7b7496d1916214e47336aa4d6b6a8fbf6602f
lex <clgo@163.com>
2024-10-13 09:32:17 +0800

    8.5# class

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,44 +1,49 @@
 <template>
-    <div class="person">
-        <h2>汽车信息：一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
-        <button @click="changePrice">修改汽车的价格</button>
-        <br>
-        <h2>游戏列表：</h2>
-        <ul>
-          <li v-for="g in games" :key="g.id">{{ g.name }}</li>
-        </ul>
-        <button @click="changeFirstGame">修改第一个游戏的名字</button>
-    </div>
+  <div class="person">
+    <h2>汽车信息：一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
+    <button @click="changePrice">修改汽车的价格</button>
+    <button @click="changeBrand">修改汽车的品牌</button>
+    <button @click="changeCar">修改汽车</button>
+    <hr>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="changeSum">点我 sum + 1</button>
+  </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { reactive } from 'vue';
+import { ref, reactive } from 'vue';
 
 // data
 let car = reactive({ brand: 'Mercedes Benz', price: 100 })
-let games = reactive([{ id: '#1', name: '王者荣耀' }, { id: '#2', name: '原神' }, { id: '#3', name: '三国志' }])
-
-// console.log(car)
+let sum = ref(0)
 
 // methods
-function changePrice() {
-    car.price += 10
+function changeCar() {
+  // car = { brand: 'Alto', price: 1 }
+  // reactive 对象直接更改后就不是响应式对象了，需要使用 Object.assign 方式来分配新的对象
+  Object.assign(car, { brand: 'Alto', price: 1 })
 }
-function changeFirstGame(){
-  games[0].name = '杀戮地带'
+function changeBrand() {
+  car.brand = ' BMW '
+}
+function changePrice() {
+  car.price += 10
+}
+function changeSum() {
+  sum.value += 1
 }
 </script>
 
 <style scoped>
 .person {
-    background-color: skyblue;
-    box-shadow: 0 0 10px;
-    border-radius: 10px;
-    padding: 20px;
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
 }
 
 button {
-    margin: 0 5px;
+  margin: 0 5px;
 }
 
 ul {

e70b8d6fc79a598e8d6125ab44b48a048f8582ff
lex <clgo@163.com>
2024-10-13 11:17:25 +0800

    11# class

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,36 +1,28 @@
 <template>
   <div class="person">
-    <h2>汽车信息：一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
-    <button @click="changePrice">修改汽车的价格</button>
-    <button @click="changeBrand">修改汽车的品牌</button>
-    <button @click="changeCar">修改汽车</button>
-    <hr>
-    <h2>当前求和为：{{ sum }}</h2>
-    <button @click="changeSum">点我 sum + 1</button>
+    <h2>姓名:{{ name }}</h2>
+    <h2>年龄:{{ age }}, {{ nl }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { ref, reactive } from 'vue';
+import { reactive, toRefs, toRef } from 'vue';
+let person = reactive({
+  name: '张三',
+  age: 18
+})
 
-// data
-let car = reactive({ brand: 'Mercedes Benz', price: 100 })
-let sum = ref(0)
+let { name, age } = toRefs(person)
+let nl = toRef(person, 'age')
+console.log(nl.value)
 
-// methods
-function changeCar() {
-  // car = { brand: 'Alto', price: 1 }
-  // reactive 对象直接更改后就不是响应式对象了，需要使用 Object.assign 方式来分配新的对象
-  Object.assign(car, { brand: 'Alto', price: 1 })
+function changeName() {
+  name.value += '~'
 }
-function changeBrand() {
-  car.brand = ' BMW '
-}
-function changePrice() {
-  car.price += 10
-}
-function changeSum() {
-  sum.value += 1
+function changeAge() {
+  age.value += 1
 }
 </script>
 

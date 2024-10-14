480d6baa589a6480dcc480d0b8fe5909c38e4d47
lex <clgo@163.com>
2024-10-14 22:05:41 +0800

    watch 情况五:监视上述的多个数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="person">
-    <h1>情况四，监视【ref】或【reactive】定义的【对象类型】数据中的某个属性</h1>
+    <h1>情况五，监视上述多个数据</h1>
     <h2>姓名: {{ person.name }}</h2>
     <h2>年龄: {{ person.age }}</h2>
     <h2>汽车: {{ person.car.c1 }}、{{ person.car.c2 }}</h2>
@@ -10,7 +10,7 @@
     <button @click="changeC2">修改第二台车</button>
     <button @click="changeCar">修改整个车</button>
   </div>
-</template>
+</template>  
 
 <script lang="ts" setup name="Person">
   import { reactive, watch } from 'vue';
@@ -44,19 +44,9 @@
     person.car = { c1: '雅迪', c2: '爱玛' }
   }
 
-  /*
-  监视 ref 或 ractive 定义的【对象类型】的数据中的某个属性：
-  1.若该属性是【基本类型】不是【对象属性】，需要写成函数形式
-  */
-  watch(() => person.name, (newValue, oldValue) => {
-    console.log('person.name 发生变化了', newValue, oldValue)
-  })
 
-  /* 
-  2.若该属性是依然是【对象类型】，可直接编，也可写成函数，最好写成函数，如果要监控具体的属性变化，加上 deep:true
-  结论：监视的要是对象里的属性，最好写函数式，若是对象监视的是地址值，需要关注对象内部需要手动开启深度监视
-  */
-  watch(() => person.car, (newValue, oldValue) => {
+  // watch，情况五:监视上述的多个数据
+  watch([()=> person.name, ()=> person.car.c1, person.car], (newValue, oldValue) => {
     console.log('person.car 发生变化了', newValue, oldValue)
   }, { deep: true })
 </script>

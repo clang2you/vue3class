fd0271ea2657ab8d66895e39ca669bcb6ad632b0
lex <clgo@163.com>
2024-10-17 22:49:43 +0800

    router 对路由的理解 & 基本切换效果

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from "vue";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 引入路由器
+import router from "./router";
+
+const app = createApp(App)
+
+// 使用路由器
+app.use(router)
+
+// 挂载整个应用到 app 容器中
+app.mount('#app')

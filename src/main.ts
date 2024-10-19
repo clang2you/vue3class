387230ce5c78e30a81dba80e6be76536f9d30e61
Lex Chen <clang2you@outlook.com>
2024-10-19 10:50:54 +0800

    搭建pinia环境

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from "vue";
 import App from './App.vue'
+// 引入 pinia
+import { createPinia } from "pinia";
 
 const app = createApp(App)
 
+// 创建 pinia
+const pinia = createPinia()
+// 安装 pinia
+app.use(pinia)
 // 挂载整个应用到 app 容器中
 app.mount('#app')
